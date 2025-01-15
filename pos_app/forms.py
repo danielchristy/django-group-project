c09cd3a5895228a9df0c28d7f8734754e0ce640f
Sean Jenkins <sjenkins@basecampcodingacademy.org>
2025-01-14 20:43:39 -0600

    Added two delete views

--- pos_app/forms.py
+++ pos_app/forms.py
@@ -6,3 +6,16 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
+
+class SignInForm(forms.Form):
+    name = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    repeated_password = forms.CharField(widget=forms.PasswordInput)
+    
+    def clean(self):
+        cleaned_data = super().clean()
+
+        if cleaned_data["password"] != cleaned_data["repeated_password"]:
+            self.add_error("repeated_password", "Passwords must match")
+
+        return cleaned_data
--- pos_app/views.py
+++ pos_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from pos_app.forms import *
+from pos_app.models import *
 
-# Create your views here.
+def delete_all_items(request):
+    if request.method == 'POST':
+        Item.objects.all().delete()
+        return HttpResponse("All items deleted")
+    return HttpResponse("Cannot perform task", status=400)
+    
+def delete_item_by_id(request, item_id):
+    if request.method == 'POST':
+        item = get_object_or_404(Item, id=item_id)
+        item.delete()
+        return HttpResponse("Item deleted")
+    return HttpResponse("Cannot perform task", status=400)
