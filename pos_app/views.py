41a78edffcf56b1e6905b1f70be350381820dbe8
Faustino Buenrostro <fbuenrostro@basecampcodingacademy.org>
2025-01-14 20:26:16 -0600

    first intial commit made basic log in and out

--- pos_app/views.py
+++ pos_app/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render,redirect
+from django.shortcuts import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+
+def login_func(request):
+    if request.method=="POST":
+        username=request.POST.get("username")
+        password=request.POST.get("password")
+
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect("home_page")
+        else:
+            messages.info(request,"username or password is incorrect")
+    return render(request, 'login.html')
+        
+
+def register_func(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+        
+
+def logout_func(request):
+    logout(request)
+    return redirect('login')
+        
+
+def home_page(request):
+    return render(request, 'home.html')
+        
